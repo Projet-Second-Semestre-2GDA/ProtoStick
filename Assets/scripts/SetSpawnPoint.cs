b0d23ab6490d9a925312fac0d42e22f7b378c206
Ueihttam <matthieu.darmagnac@laposte.net>
2020-03-02 15:32:18 +0100

    ajout du set de spawn point
    (ajout du set de spawn point)

---
+++ Assets/scripts/SetSpawnPoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetSpawnPoint : MonoBehaviour
+{
+
+    public GameObject spawnPointPlayer;
+
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            spawnPointPlayer.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        }
+    }
+
+}
