89e76c8f555afc3f3c6608b1b5771947452e6dfe
Iannis Poquet <ianpo@live.fr>
2020-04-03 18:01:28 +0200

    Nouvelle version a deux joueurs mais ne fonctionne pas il faut refaire les inputs
    J'envoie ça ici comme ça vous pouvez refaire les inputs, je peux pas changer les script avant sinon rien ne marcheras

---
+++ Assets/scripts/Player/PlayerNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerNumber : MonoBehaviour
+{
+    [HideInInspector] public int playerNumber;
+    [HideInInspector] public int playerIndex;
+    
+    private void Awake()
+    {
+        SetPlayerNumber();
+    }
+
+    private void SetPlayerNumber()
+    {
+        switch (name.Contains("One"))
+        {
+            case true:
+                playerNumber = 1;
+                break;
+            case false:
+                playerNumber = 2;
+                break;
+            default:
+                break;
+        }
+
+        playerIndex = playerNumber - 1;
+    }
+}
