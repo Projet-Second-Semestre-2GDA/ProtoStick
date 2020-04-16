445a5a7821ac4510dfe0516fb359da4553ebe9e4
Iannis Poquet <ianpo@live.fr>
2020-04-16 16:51:23 +0200

    Ajout d'un system de fin de niveau a compléter

---
+++ Assets/scripts/Autre/Ending.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ending : MonoBehaviour
+{
+    private bool raceIsOver;
+
+    private string winnerName;
+
+    private GameObject canvas;
+    private Text text;
+    
+    void Awake()
+    {
+        raceIsOver = false;
+        winnerName = "";
+    }
+
+    private void Start()
+    {
+        canvas = GetComponentInChildren<Canvas>().gameObject;
+        text = GetComponentInChildren<Text>();
+        canvas.SetActive(false);
+    }
+
+    private void SetWin(string name)
+    {
+        Debug.LogWarning("Le winner est " + name);
+        canvas.SetActive(true);
+        text.text = "The Winner is\n" + name;
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.attachedRigidbody.CompareTag("Player") && !raceIsOver)
+        {
+            raceIsOver = true;
+            winnerName = other.attachedRigidbody.gameObject.name;
+            SetWin(winnerName);
+        }
+    }
+}
