8584446eb9ade8d8714e59d06ac84e102157d63b
Iannis Poquet <ianpo@live.fr>
2020-05-20 18:42:16 +0200

    Les portes simple fonctionnent

---
+++ Assets/Scripts/Autre/Obstacles/SimpleDoor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleDoor : Interactible
+{
+    public override void Activate()
+    {
+        base.Activate();
+        gameObject.SetActive(false);
+    }
+}
