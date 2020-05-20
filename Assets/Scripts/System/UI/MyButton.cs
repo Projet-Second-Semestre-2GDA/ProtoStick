0cabd7a51fdc55fd6dec89a1b653e85b23704821
Iannis Poquet <ianpo@live.fr>
2020-05-20 17:35:42 +0200

    Scroll du menu level réaliser

---
+++ Assets/Scripts/System/UI/MyButton.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class MyButton : Button
+{
+    private RectTransform content;
+    private bool isSelected; //Boolean qui me sert à savoir si le bouton est selectionner.
+
+    protected override void Awake()
+    {
+        base.Awake();
+        isSelected = false;
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        content = GameObject.FindGameObjectWithTag("ScrollViewer").GetComponent<RectTransform>();
+        //Cette ligne permet de différencier mes différents bouton à l'écran lorsque je test le scroll
+        // GetComponentInChildren<TextMeshProUGUI>().text += " - " + Random.value;
+    }
+    
+    public override void OnSelect(BaseEventData eventData)
+    {
+        base.OnSelect(eventData);
+        isSelected = true;
+    }
+
+    public override void OnDeselect(BaseEventData eventData)
+    {
+        base.OnDeselect(eventData);
+        isSelected = false;
+    }
+
+    private void Update()
+    {
+        if (isSelected)
+        {
+            //Si le bouton est selectionner, je vérifier ma position
+            //Si je suis en dehors du cadre, je fait un translate au parent
+            //(Les valeurs ont été écrite en durrrr pour correspondre à ce cas précis.)
+            var contentY = content.transform.localPosition.y;
+            var y = transform.localPosition.y;
+            var realY = contentY + y;
+            Debug.Log(contentY + " + " + y + " = " + realY);
+            if (realY > 0)
+            {
+                content.Translate(0,-455,0);
+            }
+            else if (realY < -1080)
+            {
+                content.Translate(0,455,0);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/System/UI/Testeur.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testeur : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        var trans = transform;
+        var pos = trans.position;
+        pos.y = 1080;
+        trans.position = pos;
+    }
+}
