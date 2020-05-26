9dfaf45f2c369795991b83e992c48e9e5105cfb7
Iannis Poquet <ianpo@live.fr>
2020-05-26 15:19:17 +0200

    micro Changement

--- Assets/Scripts/Autre/AccelerationFeedback.cs
+++ Assets/Scripts/Autre/AccelerationFeedback.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class AccelerationFeedback : MonoBehaviour
 {
 
-    public UPS uPS;
+    private UPS uPS;
 
     private FMOD.Studio.EventInstance joueurVitesse;
     private FMOD.Studio.EventDescription joueurVitesseDescription;
@@ -25,25 +25,13 @@ public class AccelerationFeedback : MonoBehaviour
 
         joueurVitesse = FMODUnity.RuntimeManager.CreateInstance("event:/DA glitch/Personnage longiforme/joueur_vitesse");
         joueurVitesse.start();
-
-        joueurVitesseDescription = FMODUnity.RuntimeManager.GetEventDescription("event:/DA glitch/Personnage longiforme/joueur_vitesse");
-        //Debug.Log(joueurVitesseDescription);
-
-        joueurVitesseDescription.getParameterDescriptionByName("acceleration_player", out accelerationDescription);
-
-        accelerationID = accelerationDescription.id;
-        Debug.Log(accelerationID.data1);
-        Debug.Log(accelerationID.data2);
     }
 
 
     private void Update()
     {
-        
         speedNumber = uPS.actualsUPSPlayer;
-        //Debug.Log("speednumber =" + speedNumber);
-
-        joueurVitesse.setParameterByID(accelerationID, speedNumber);
+        joueurVitesse.setParameterByName("acceleration_player", speedNumber);
     }
 
 }
