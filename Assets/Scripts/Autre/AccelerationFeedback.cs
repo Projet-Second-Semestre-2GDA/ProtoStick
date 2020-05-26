32ebd1039015c7e8b38c0508c97876aff469956e
Iannis Poquet <ianpo@live.fr>
2020-05-26 16:59:37 +0200

    Lower the emission

--- Assets/Scripts/Autre/AccelerationFeedback.cs
+++ Assets/Scripts/Autre/AccelerationFeedback.cs
@@ -24,14 +24,17 @@ public class AccelerationFeedback : MonoBehaviour
     private void Start()
     {
         uPS = GetComponent<UPS>();
-
+        // FMODUnity.RuntimeManager.
         joueurVitesse = FMODUnity.RuntimeManager.CreateInstance("event:/DA glitch/Personnage longiforme/joueur_vitesse");
+        joueurVitesse.setVolume(5);
         
+
     }
 
 
     private void Update()
     {
+        joueurVitesse.setVolume(5);
         speedNumber = uPS.actualsUPSPlayer;
         var result = joueurVitesse.setParameterByName("acceleration_player_flanger", speedNumber);
         var result2 = joueurVitesse.setParameterByName("acceleration_player_volume", speedNumber);
@@ -39,10 +42,33 @@ public class AccelerationFeedback : MonoBehaviour
         Debug.Log("player speed volume : " + result2);
         PLAYBACK_STATE state;
         joueurVitesse.getPlaybackState(out state);
-
+        Debug.Log("State : " + state);
         if (state == PLAYBACK_STATE.STOPPED)
         {
             joueurVitesse.start();
+        }
+        else
+        {
+            float accelerationPlayerFlanger;
+            joueurVitesse.getParameterByName("acceleration_player_flanger", out accelerationPlayerFlanger);
+            Debug.Log("acceleration_player_flanger : " + accelerationPlayerFlanger);
+            
+            float accelerationPlayerVolume;
+            joueurVitesse.getParameterByName("acceleration_player_volume", out accelerationPlayerVolume);
+            Debug.Log("acceleration_player_volume : " + accelerationPlayerVolume);
+            
+            float volume;
+            float finalVolume;
+            joueurVitesse.getVolume(out volume,out finalVolume);
+            Debug.Log("Volume : " + volume);
+            Debug.Log("Final volume : " + finalVolume);
+
+
+            float pitch;
+            float finalPitch;
+            joueurVitesse.getPitch(out pitch,out finalPitch);
+            Debug.Log("Pitch : " + pitch);
+            Debug.Log("Final Pitch : " + finalPitch);
         }
     }
 
