348b6853b4d6c42375d8567071597c62ce645f3d
Ueihttam <matthieu.darmagnac@laposte.net>
2020-05-28 19:39:43 +0200

    petits changements

---
+++ Assets/SpamMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpamMovement : MonoBehaviour
+{
+    Animator playerAnimation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnimation = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerAnimation.SetFloat("Walk", 1);
+    }
+}
