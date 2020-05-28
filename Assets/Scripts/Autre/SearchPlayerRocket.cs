da1cd0ca10baa229196e97143c603c9e32b19210
Iannis Poquet <ianpo@live.fr>
2020-05-28 23:55:21 +0200

    Changement dans la rocket

--- Assets/Scripts/Autre/SearchPlayerRocket.cs
+++ Assets/Scripts/Autre/SearchPlayerRocket.cs
@@ -2,12 +2,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class SearchPlayerRocket : MonoBehaviour
 {
     private float speedMax;
     private Rigidbody rb;
-    private float toAddForce = 200;
+    [FormerlySerializedAs("toAddForce")] [SerializeField] private float multiplicator = 1f;
     private void Start()
     {
         speedMax = (rb = GetComponentInParent<Rigidbody>()).velocity.magnitude;
@@ -17,15 +18,17 @@ public class SearchPlayerRocket : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+            Debug.Log("I found " + other.attachedRigidbody.name + " and go to him !!");
             var playerPos = other.attachedRigidbody.transform.position;
             speedMax = rb.velocity.magnitude;
 
             var direction = playerPos - transform.position;
-            
-            rb.AddForce(direction.normalized*toAddForce,ForceMode.Force);
-            
             var velocity = rb.velocity;
-            rb.velocity = (velocity.magnitude > speedMax) ? velocity.normalized * speedMax : velocity;
+            velocity += direction.normalized * (speedMax* multiplicator * Time.fixedDeltaTime);
+            // rb.AddForce(direction.normalized*multiplicator,ForceMode.Force);
+            
+            rb.velocity = (velocity = velocity.normalized * speedMax);
+            rb.transform.LookAt(transform.position + velocity);
         }
     }
 }
